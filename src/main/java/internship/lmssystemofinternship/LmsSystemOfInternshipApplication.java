996c65fb905119359d57e9de65b294eba3f5de89
MASTANVALI9 <153994082+MASTANVALI9@users.noreply.github.com>
2025-09-27 21:07:45 +0530

    lms project phase day1

---
+++ src/main/java/internship/lmssystemofinternship/LmsSystemOfInternshipApplication.java
@@ -0,0 +1,13 @@
+package internship.lmssystemofinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSystemOfInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsSystemOfInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/internship/lmssystemofinternship/LmsSystemOfInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package internship.lmssystemofinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemOfInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
